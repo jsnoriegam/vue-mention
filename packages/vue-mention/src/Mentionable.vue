2f4206c0d7523dd36bc23b78509565a2a994cc6a
Juan Salvador <jsnoreigam@latinosoft.co>
2022-02-16 18:56:16 -0500

    Experiments 3

--- packages/vue-mention/src/Mentionable.vue
+++ packages/vue-mention/src/Mentionable.vue
@@ -1,19 +1,29 @@
 <script lang="ts">
-import getCaretPosition from 'textarea-caret'
-import { Dropdown, options } from 'floating-vue'
-import { defineComponent, computed, onMounted, onUnmounted, onUpdated, ref, watch, nextTick, PropType } from 'vue'
+import getCaretPosition from "textarea-caret";
+import { Dropdown, options } from "floating-vue";
+import {
+  defineComponent,
+  computed,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+  watch,
+  nextTick,
+  PropType,
+} from "vue";
 
 options.themes.mentionable = {
-  $extend: 'dropdown',
-  placement: 'top-start',
+  $extend: "dropdown",
+  placement: "top-start",
   arrowPadding: 6,
   arrowOverflow: false,
-}
+};
 
 export interface MentionItem {
-  searchText?: string
-  label?: string
-  value: any
+  searchText?: string;
+  label?: string;
+  value: any;
 }
 
 export default defineComponent({
@@ -61,7 +71,7 @@ export default defineComponent({
 
     theme: {
       type: String,
-      default: 'mentionable',
+      default: "mentionable",
     },
 
     caretHeight: {
@@ -70,292 +80,340 @@ export default defineComponent({
     },
   },
 
-  emits: ['search', 'open', 'close', 'apply'],
+  emits: ["search", "open", "close", "apply"],
 
-  setup (props, { emit }) {
-    const currentKey = ref<string>(null)
-    let currentKeyIndex: number
-    const oldKey = ref<string>(null)
+  setup(props, { emit }) {
+    const currentKey = ref<string>(null);
+    let currentKeyIndex: number;
+    const oldKey = ref<string>(null);
 
     // Items
 
-    const searchText = ref<string>(null)
+    const searchText = ref<string>(null);
 
     watch(searchText, (value, oldValue) => {
       if (value) {
-        emit('search', value, oldValue)
+        emit("search", value, oldValue);
       }
-    })
+    });
 
     const filteredItems = computed(() => {
       if (!searchText.value || props.filteringDisabled) {
-        return props.items
+        return props.items;
       }
 
-      const finalSearchText = searchText.value.toLowerCase()
+      const finalSearchText = searchText.value.toLowerCase();
 
-      return props.items.filter(item => {
-        let text: string
+      return props.items.filter((item) => {
+        let text: string;
         if (item.searchText) {
-          text = item.searchText
+          text = item.searchText;
         } else if (item.label) {
-          text = item.label
+          text = item.label;
         } else {
-          text = ''
+          text = "";
           for (const key in item) {
-            text += item[key]
+            text += item[key];
           }
         }
-        return text.toLowerCase().includes(finalSearchText)
-      })
-    })
+        return text.toLowerCase().includes(finalSearchText);
+      });
+    });
 
-    const displayedItems = computed(() => filteredItems.value.slice(0, props.limit))
+    const displayedItems = computed(() =>
+      filteredItems.value.slice(0, props.limit)
+    );
 
     // Selection
 
-    const selectedIndex = ref(0)
+    const selectedIndex = ref(0);
 
-    watch(displayedItems, () => {
-      selectedIndex.value = 0
-    }, {
-      deep: true,
-    })
+    watch(
+      displayedItems,
+      () => {
+        selectedIndex.value = 0;
+      },
+      {
+        deep: true,
+      }
+    );
 
     // Input element management
 
-    let input: HTMLElement
-    const el = ref<HTMLDivElement>(null)
+    let input: HTMLElement;
+    const el = ref<HTMLDivElement>(null);
 
-    function getInput () {
-      return el.value.querySelector('input') ?? el.value.querySelector('textarea') ?? el.value.querySelector('[contenteditable="true"]')
+    function getInput() {
+      return (
+        el.value.querySelector("input") ??
+        el.value.querySelector("textarea") ??
+        el.value.querySelector('[contenteditable="true"]')
+      );
     }
 
     onMounted(() => {
-      input = getInput()
-      attach()
-    })
+      input = getInput();
+      attach();
+    });
 
     onUpdated(() => {
-      const newInput = getInput()
+      const newInput = getInput();
       if (newInput !== input) {
-        detach()
-        input = newInput
-        attach()
+        detach();
+        input = newInput;
+        attach();
       }
-    })
+    });
 
     onUnmounted(() => {
-      detach()
-    })
+      detach();
+    });
 
     // Events
 
-    function attach () {
+    function attach() {
       if (input) {
-        input.addEventListener('input', onInput)
-        input.addEventListener('keydown', onKeyDown)
-        input.addEventListener('keyup', onKeyUp)
-        input.addEventListener('scroll', onScroll)
-        input.addEventListener('blur', onBlur)
+        input.addEventListener("input", onInput);
+        input.addEventListener("keydown", onKeyDown);
+        input.addEventListener("keyup", onKeyUp);
+        input.addEventListener("scroll", onScroll);
+        input.addEventListener("blur", onBlur);
       }
     }
 
-    function detach () {
+    function detach() {
       if (input) {
-        input.removeEventListener('input', onInput)
-        input.removeEventListener('keydown', onKeyDown)
-        input.removeEventListener('keyup', onKeyUp)
-        input.removeEventListener('scroll', onScroll)
-        input.removeEventListener('blur', onBlur)
+        input.removeEventListener("input", onInput);
+        input.removeEventListener("keydown", onKeyDown);
+        input.removeEventListener("keyup", onKeyUp);
+        input.removeEventListener("scroll", onScroll);
+        input.removeEventListener("blur", onBlur);
       }
     }
 
-    function onInput () {
-      checkKey()
+    function onInput() {
+      checkKey();
     }
 
-    function onBlur () {
-      closeMenu()
+    function onBlur() {
+      closeMenu();
     }
 
-    function onKeyDown (e: KeyboardEvent) {
+    function onKeyDown(e: KeyboardEvent) {
       if (currentKey.value) {
-        if (e.key === 'ArrowDown') {
-          selectedIndex.value++
+        if (e.key === "ArrowDown") {
+          selectedIndex.value++;
           if (selectedIndex.value >= displayedItems.value.length) {
-            selectedIndex.value = 0
+            selectedIndex.value = 0;
           }
-          cancelEvent(e)
+          cancelEvent(e);
         }
-        if (e.key === 'ArrowUp') {
-          selectedIndex.value--
+        if (e.key === "ArrowUp") {
+          selectedIndex.value--;
           if (selectedIndex.value < 0) {
-            selectedIndex.value = displayedItems.value.length - 1
+            selectedIndex.value = displayedItems.value.length - 1;
           }
-          cancelEvent(e)
+          cancelEvent(e);
         }
-        if ((e.key === 'Enter' || e.key === 'Tab') &&
-          displayedItems.value.length > 0) {
-          applyMention(selectedIndex.value)
-          cancelEvent(e)
+        if (
+          (e.key === "Enter" || e.key === "Tab") &&
+          displayedItems.value.length > 0
+        ) {
+          applyMention(selectedIndex.value);
+          cancelEvent(e);
         }
-        if (e.key === 'Escape') {
-          closeMenu()
-          cancelEvent(e)
+        if (e.key === "Escape") {
+          closeMenu();
+          cancelEvent(e);
         }
       }
     }
 
-    let cancelKeyUp = null
+    let cancelKeyUp = null;
 
-    function onKeyUp (e: KeyboardEvent) {
+    function onKeyUp(e: KeyboardEvent) {
       if (cancelKeyUp && e.key === cancelKeyUp) {
-        cancelEvent(e)
+        cancelEvent(e);
       }
-      cancelKeyUp = null
+      cancelKeyUp = null;
     }
 
-    function cancelEvent (e: KeyboardEvent) {
-      e.preventDefault()
-      e.stopPropagation()
-      cancelKeyUp = e.key
+    function cancelEvent(e: KeyboardEvent) {
+      e.preventDefault();
+      e.stopPropagation();
+      cancelKeyUp = e.key;
     }
 
-    function onScroll () {
-      updateCaretPosition()
+    function onScroll() {
+      updateCaretPosition();
     }
 
-    function getSelectionStart () {
-      return input.isContentEditable ? window.getSelection().anchorOffset : (input as HTMLInputElement).selectionStart
+    function getSelectionStart() {
+      return input.isContentEditable
+        ? window.getSelection().anchorOffset
+        : (input as HTMLInputElement).selectionStart;
     }
 
-    function setCaretPosition (index: number) {
+    function setCaretPosition(index: number) {
       nextTick(() => {
-        (input as HTMLInputElement).selectionEnd = index
-      })
+        (input as HTMLInputElement).selectionEnd = index;
+      });
     }
 
-    function getValue () {
-      return input.isContentEditable ? window.getSelection().anchorNode.textContent : (input as HTMLInputElement).value
+    function getValue() {
+      return input.isContentEditable
+        ? window.getSelection().anchorNode.textContent
+        : (input as HTMLInputElement).value;
     }
 
-    function setValue (value) {
-      (input as HTMLInputElement).value = value
-      emitInputEvent('input')
+    function setValue(value) {
+      (input as HTMLInputElement).value = value;
+      emitInputEvent("input");
     }
 
-    function emitInputEvent (type: string) {
-      input.dispatchEvent(new Event(type))
+    function emitInputEvent(type: string) {
+      input.dispatchEvent(new Event(type));
     }
 
-    let lastSearchText: string = null
+    let lastSearchText: string = null;
 
-    function checkKey () {
-      const index = getSelectionStart()
+    function checkKey() {
+      const index = getSelectionStart();
       if (index >= 0) {
-        const { key, keyIndex } = getLastKeyBeforeCaret(index)
-        const text = lastSearchText = getLastSearchText(index, keyIndex)
+        const { key, keyIndex } = getLastKeyBeforeCaret(index);
+        const text = (lastSearchText = getLastSearchText(index, keyIndex));
         //if (!(keyIndex < 1 || /[\s\(\)\{\}\[\]]/.test(getValue()[keyIndex - 1]))) {
         //  return false
         //}
         if (text != null) {
-          openMenu(key, keyIndex)
-          searchText.value = text
-          return true
+          openMenu(key, keyIndex);
+          searchText.value = text;
+          return true;
         }
       }
-      closeMenu()
-      return false
+      closeMenu();
+      return false;
     }
 
-    function getLastKeyBeforeCaret (caretIndex: number) {
-      const [keyData] = props.keys.map(key => ({
-        key,
-        keyIndex: getValue().lastIndexOf(key, caretIndex - 1),
-      })).sort((a, b) => b.keyIndex - a.keyIndex)
-      return keyData
+    function getLastKeyBeforeCaret(caretIndex: number) {
+      const [keyData] = props.keys
+        .map((key) => ({
+          key,
+          keyIndex: getValue().lastIndexOf(key, caretIndex - 1),
+        }))
+        .sort((a, b) => b.keyIndex - a.keyIndex);
+      return keyData;
     }
 
-    function getLastSearchText (caretIndex: number, keyIndex: number) {
+    function getLastSearchText(caretIndex: number, keyIndex: number) {
       if (keyIndex !== -1) {
-        const text = getValue().substring(keyIndex + 1, caretIndex)
+        const text = getValue().substring(keyIndex + 1, caretIndex);
         // If there is a space we close the menu
         if (!/[\s\(\)\{\}\[\]]/.test(text)) {
-          return text
+          return text;
         }
       }
-      return null
+      return null;
     }
 
     // Position of the popper
 
-    const caretPosition = ref<{ top: number, left: number, height: number }>(null)
+    const caretPosition =
+      ref<{ top: number; left: number; height: number }>(null);
 
-    function updateCaretPosition () {
+    function updateCaretPosition() {
       if (currentKey.value) {
         if (input.isContentEditable) {
-          const rect = window.getSelection().getRangeAt(0).getBoundingClientRect()
-          const inputRect = input.getBoundingClientRect()
+          const rect = window
+            .getSelection()
+            .getRangeAt(0)
+            .getBoundingClientRect();
+          const inputRect = input.getBoundingClientRect();
           caretPosition.value = {
             left: rect.left - inputRect.left,
             top: rect.top - inputRect.top,
             height: rect.height,
-          }
+          };
         } else {
-          caretPosition.value = getCaretPosition(input, currentKeyIndex)
+          caretPosition.value = getCaretPosition(input, currentKeyIndex);
         }
-        caretPosition.value.top -= input.scrollTop
+        caretPosition.value.top -= input.scrollTop;
         if (props.caretHeight) {
-          caretPosition.value.height = props.caretHeight
+          caretPosition.value.height = props.caretHeight;
         } else if (isNaN(caretPosition.value.height)) {
-          caretPosition.value.height = 16
+          caretPosition.value.height = 16;
         }
       }
     }
 
     // Open/close
 
-    function openMenu (key: string, keyIndex: number) {
+    function openMenu(key: string, keyIndex: number) {
       if (currentKey.value !== key) {
-        currentKey.value = key
-        currentKeyIndex = keyIndex
-        updateCaretPosition()
-        selectedIndex.value = 0
-        emit('open', currentKey.value)
+        currentKey.value = key;
+        currentKeyIndex = keyIndex;
+        updateCaretPosition();
+        selectedIndex.value = 0;
+        emit("open", currentKey.value);
       }
     }
 
-    function closeMenu () {
+    function closeMenu() {
       if (currentKey.value != null) {
-        oldKey.value = currentKey.value
-        currentKey.value = null
-        emit('close', oldKey.value)
+        oldKey.value = currentKey.value;
+        currentKey.value = null;
+        emit("close", oldKey.value);
       }
     }
 
     // Apply
 
-    function applyMention (itemIndex: number) {
-      const item = displayedItems.value[itemIndex]
-      const value = (props.omitKey ? '' : currentKey.value) + String(props.mapInsert ? props.mapInsert(item, currentKey.value) : item.value) + (props.insertSpace ? ' ' : '')
+    function applyMention(itemIndex: number) {
+      const item = displayedItems.value[itemIndex];
+      const value =
+        (props.omitKey ? "" : currentKey.value) +
+        String(
+          props.mapInsert ? props.mapInsert(item, currentKey.value) : item.value
+        ) +
+        (props.insertSpace ? " " : "");
       if (input.isContentEditable) {
-        const range = window.getSelection().getRangeAt(0)
-        range.setStart(range.startContainer, range.startOffset - currentKey.value.length - (lastSearchText ? lastSearchText.length : 0))
-        range.deleteContents()
-        range.insertNode(document.createTextNode(value))
-        range.setStart(range.endContainer, range.endOffset)
-        emitInputEvent('input')
+        const range = window.getSelection()!.getRangeAt(0);
+        range.setStart(
+          range.startContainer,
+          range.startOffset -
+            currentKey.value.length -
+            (lastSearchText ? lastSearchText.length : 0)
+        );
+        range.deleteContents();
+        range.insertNode(document.createTextNode(value));
+        range.setStart(range.endContainer, range.endOffset);
+        emitInputEvent("input");
       } else {
-        setValue(replaceText(getValue(), searchText.value, value, currentKeyIndex))
-        setCaretPosition(currentKeyIndex + value.length)
+        setValue(
+          replaceText(
+            getValue() || "",
+            searchText.value,
+            value,
+            currentKeyIndex
+          )
+        );
+        setCaretPosition(currentKeyIndex + value.length);
       }
-      emit('apply', item, currentKey.value, value)
-      closeMenu()
+      emit("apply", item, currentKey.value, value);
+      closeMenu();
     }
 
-    function replaceText (text: string, searchString: string, newText: string, index: number) {
-      return text.slice(0, index) + newText + text.slice(index + searchString.length + 1, text.length)
+    function replaceText(
+      text: string,
+      searchString: string,
+      newText: string,
+      index: number
+    ) {
+      return (
+        text.slice(0, index) +
+        newText +
+        text.slice(index + searchString.length + 1, text.length)
+      );
     }
 
     return {
@@ -366,9 +424,9 @@ export default defineComponent({
       displayedItems,
       selectedIndex,
       applyMention,
-    }
+    };
   },
-})
+});
 </script>
 
 <template>
@@ -376,7 +434,7 @@ export default defineComponent({
     ref="el"
     class="mentionable"
     :class="$attrs.class"
-    style="position:relative;"
+    style="position: relative"
   >
     <slot />
 
@@ -388,23 +446,29 @@ export default defineComponent({
       :auto-hide="false"
       :theme="theme"
       class="popper"
-      style="position:absolute;"
-      :style="caretPosition ? {
-        top: `${caretPosition.top}px`,
-        left: `${caretPosition.left}px`,
-      } : {}"
+      style="position: absolute"
+      :style="
+        caretPosition
+          ? {
+              top: `${caretPosition.top}px`,
+              left: `${caretPosition.left}px`,
+            }
+          : {}
+      "
     >
       <div
-        :style="caretPosition ? {
-          height: `${caretPosition.height}px`,
-        } : {}"
+        :style="
+          caretPosition
+            ? {
+                height: `${caretPosition.height}px`,
+              }
+            : {}
+        "
       />
 
       <template #popper>
         <div v-if="!displayedItems.length">
-          <slot name="no-result">
-            No result
-          </slot>
+          <slot name="no-result"> No result </slot>
         </div>
 
         <template v-else>
@@ -423,11 +487,7 @@ export default defineComponent({
               :item="item"
               :index="index"
             >
-              <slot
-                name="item"
-                :item="item"
-                :index="index"
-              >
+              <slot name="item" :item="item" :index="index">
                 {{ item.label || item.value }}
               </slot>
             </slot>
